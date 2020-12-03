8464893ccb37b610cf437eff18c11b8bf203326f
redha-boubakour <redha.boubakour@yahoo.fr>
2020-12-03 13:59:00 +0100

    Class Request

--- config/Router.php
+++ config/Router.php
@@ -11,24 +11,27 @@ class Router
     private $frontController;
     private $backController;
     private $errorController;
+    private $request;
 
     public function __construct()
     {
         $this->frontController = new FrontController();
         $this->backController = new BackController();
         $this->errorController = new ErrorController();
+        $this->request = new Request();
     }
 
     public function run()
     {
+        $route = $this->request->getGet()->get('route');
         try {
-            if(isset($_GET['route']))
+            if(isset($route))
             {
-                if ($_GET['route'] === 'article'){
-                    $this->frontController->article($_GET['articleId']);
+                if ($route === 'article'){
+                    $this->frontController->article($this->request->getGet()->get('articleId'));
                 } 
-                elseif ($_GET['route'] === 'addArticle') {
-                    $this->backController->addArticle($_POST);
+                elseif ($route === 'addArticle') {
+                    $this->backController->addArticle($this->request->getPost());
                 }
                 else {
                     $this->errorController->errorNotFound();
--- src/DAO/ArticleDAO.php
+++ src/DAO/ArticleDAO.php
@@ -2,6 +2,7 @@
 
 namespace App\src\DAO;
 
+use App\config\Parameter;
 use App\src\model\Article;
 
 class ArticleDAO extends DAO
@@ -39,10 +40,10 @@ class ArticleDAO extends DAO
         return $this->buildObject($article);
     }
 
-    public function addArticle($article)
+    public function addArticle(Parameter $post)
     {
         extract($article);
         $sql = 'INSERT INTO article (title, content, author, createdAt) VALUES (?, ?, ?, NOW())';
-        $this->createQuery($sql, [$title, $content, $author]);
+        $this->createQuery($sql, [$post->get('title'), $post->get('content'), $post->get('author')]);
     }
 }
