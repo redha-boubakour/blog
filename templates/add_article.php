54fbcf512857a8b19fba2afcb50a8fe513feda22
redha-boubakour <redha.boubakour@yahoo.fr>
2020-12-04 11:53:56 +0100

    form_article (base)

--- templates/add_article.php
+++ templates/add_article.php
@@ -1,17 +1,8 @@
 <?php $this->title = "Nouvel article"; ?>
 
-<h1>Mon blog</h1>
+<h1>Blog</h1>
 
 <div>
-    <form method="post" action="../public/index.php?route=addArticle">
-        <label for="title">Titre</label><br>
-        <input type="text" id="title" name="title"><br>
-        <label for="content">Contenu</label><br>
-        <textarea id="content" name="content"></textarea><br>
-        <label for="author">Auteur</label><br>
-        <input type="text" id="author" name="author"><br><br>
-        <input type="submit" value="Envoyer" id="submit" name="submit">
-    </form>
-
-    <br><a href="../public/index.php">Retour à l'accueil</a>
+    <?php include('form_article.php');?>
+    <a href="../public/index.php">Retour à l'accueil</a>
 </div>
--- templates/edit_article.php
+++ templates/edit_article.php
@@ -1,17 +1,8 @@
 <?php $this->title = "Modifier l'article"; ?>
 
-<h1>Mon blog</h1>
+<h1>Blog</h1>
 
 <div>
-    <form method="post" action="../public/index.php?route=editArticle&articleId=<?= htmlspecialchars($article->getId()); ?>">
-        <label for="title">Titre</label><br>
-        <input type="text" id="title" name="title" value="<?= htmlspecialchars($article->getTitle()); ?>"><br>
-        <label for="content">Contenu</label><br>
-        <textarea id="content" name="content"><?= htmlspecialchars($article->getContent()); ?></textarea><br>
-        <label for="author">Auteur</label><br>
-        <input type="text" id="author" name="author" value="<?= htmlspecialchars($article->getAuthor()); ?>"><br><br>
-        <input type="submit" value="Mettre à jour" id="submit" name="submit">
-    </form>
-
-    <br><a href="../public/index.php">Retour à l'accueil</a>
+    <?php include('form_article.php');?>
+    <a href="../public/index.php">Retour à l'accueil</a>
 </div>
